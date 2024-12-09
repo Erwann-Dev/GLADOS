449612f26b1c73005f0c7d32d6304ea5c82f8467
aurelielefr <aurelie.lefranc@epitech.eu>
2024-12-09 17:45:22 +0100

    [FEAT] check file, take content args file || stdout file

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,38 @@
 module Main (main) where
 
-import Lib
+import System.Exit
+import System.Environment
+import System.IO
+
+checkTypeFile :: String -> Bool
+checkTypeFile file_path = 
+    let file_extension = reverse $ takeWhile (/= '.') $ reverse file_path
+    in file_extension == "scm"
+
+checkPathFile :: [String] -> IO (Maybe String)
+checkPathFile args = case args of
+    [] -> return Nothing
+    [path] -> 
+        if not $ checkTypeFile path then do
+            hPutStrLn stderr "Error: wrong file extension"
+            exitWith (ExitFailure 84)
+        else return $ Just path
+    _ -> do
+        hPutStrLn stderr "Error: too many arguments"
+        exitWith (ExitFailure 84)
+
+tryReadFile :: String -> IO String
+tryReadFile path = readFile path
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    filePath <- checkPathFile args
+    case filePath of 
+        Just path -> do
+            contents <- tryReadFile path
+            putStrLn contents
+        Nothing -> do
+            contents <- getContents
+            putStrLn contents
+
