4cecbec4f8ad3755d8c7cac3a6e6bbf0984cad0c
mck@fedora <micka.daoud@epitech.eu>
2025-01-21 15:57:11 +0100

    [FEAT] compiler error tracking using ExceptT

--- src/ASTCompiler.hs
+++ src/ASTCompiler.hs
@@ -5,50 +5,59 @@ module ASTCompiler (
   compile,
   CompilerState (..),
   initialCompilerState,
+  CompilerError (..),
 ) where
 
 import AST
 import Control.Monad
+import Control.Monad.Except
 import Control.Monad.State
 import qualified Data.Map as Map
 
--- Bytecode instruction set
-data Instruction
-  = PushInt Int -- Push integer constant
-  | PushFloat Float -- Push float constant
-  | PushNull -- Push null value
-  | LoadVar String -- Load variable from environment
-  | StoreVar String -- Store value in variable
-  | DefineVar String Bool -- Define new variable (with mutability flag)
-  | Pop -- Pop value from stack
-  | Add -- Add top two values
-  | Sub -- Subtract
-  | Mul -- Multiply
-  | Div -- Divide
-  | Mod -- Modulo
-  | Not -- Logical not
-  | And -- Logical and
-  | Or -- Logical or
-  | Equal -- Equality comparison
-  | NotEqual -- Inequality comparison
-  | Greater -- Greater than
-  | Less -- Less than
-  | GreaterEqual -- Greater than or equal
-  | LessEqual -- Less than or equal
-  | JumpIfFalse Int -- Conditional jump
-  | Jump Int -- Unconditional jump
-  | Call Int -- Function call with arg count
-  | Return -- Return from function
-  | MakeArray Int -- Create array with n elements
-  | LoadArray -- Load array element
-  | Print -- Print value
+-- Define specific error types
+data CompilerError
+  = NotImplementedError String
+  | ScopeError String
+  | TypeError String
+  | CompilationError String
   deriving (Show, Eq)
 
--- Compiler state
+-- Bytecode instruction set
+data Instruction
+  = PushInt Int
+  | PushFloat Float
+  | PushNull
+  | LoadVar String
+  | StoreVar String
+  | DefineVar String Bool
+  | Pop
+  | Add
+  | Sub
+  | Mul
+  | Div
+  | Mod
+  | Not
+  | And
+  | Or
+  | Equal
+  | NotEqual
+  | Greater
+  | Less
+  | GreaterEqual
+  | LessEqual
+  | JumpIfFalse Int
+  | Jump Int
+  | Call Int
+  | Return
+  | MakeArray Int
+  | LoadArray
+  | Print
+  deriving (Show, Eq)
+
 data CompilerState = CompilerState
-  { instructions :: [Instruction] -- Generated instructions
-  , labelCounter :: Int -- Counter for generating unique labels
-  , scopeStack :: [Map.Map String Int] -- Stack of variable scopes
+  { instructions :: [Instruction]
+  , labelCounter :: Int
+  , scopeStack :: [Map.Map String Int]
   }
   deriving (Show)
 
@@ -60,13 +69,16 @@ initialCompilerState =
     , scopeStack = [Map.empty]
     }
 
-type Compiler a = State CompilerState a
+-- New type for our compiler monad stack
+type Compiler a = ExceptT CompilerError (State CompilerState) a
 
--- Main compilation function
-compile :: Node -> ([Instruction], CompilerState)
-compile node = runState instructionList initialCompilerState
+-- Main compilation function now returns Either
+compile :: Node -> Either CompilerError [Instruction]
+compile node =
+  let (result, _) = runState (runExceptT compileProgram) initialCompilerState
+   in result
  where
-  instructionList = do
+  compileProgram = do
     compileNode node
     gets instructions
 
@@ -81,7 +93,7 @@ newLabel = do
   modify $ \s -> s{labelCounter = labelCounter s + 1}
   return current
 
--- Compilation for each node type
+-- Compilation for each node type with proper error handling
 compileNode :: Node -> Compiler ()
 compileNode = \case
   IntV n -> emit $ PushInt n
@@ -106,12 +118,10 @@ compileNode = \case
   If cond thenExpr mElseExpr -> do
     endLabel <- newLabel
     elseLabel <- newLabel
-
     compileNode cond
     emit $ JumpIfFalse elseLabel
     compileNode thenExpr
     emit $ Jump endLabel
-
     emit $ JumpIfFalse elseLabel
     case mElseExpr of
       Just elseExpr -> compileNode elseExpr
@@ -120,7 +130,6 @@ compileNode = \case
   While cond body -> do
     startLabel <- newLabel
     endLabel <- newLabel
-
     emit $ Jump startLabel
     compileNode cond
     emit $ JumpIfFalse endLabel
@@ -149,7 +158,44 @@ compileNode = \case
   LtOp e1 e2 -> compileBinaryOp e1 e2 Less
   GeqOp e1 e2 -> compileBinaryOp e1 e2 GreaterEqual
   LeqOp e1 e2 -> compileBinaryOp e1 e2 LessEqual
-  _ -> error "Compilation not implemented"
+  -- Unimplemented features with proper error messages
+  For{} ->
+    throwError $ NotImplementedError "For loops are not yet implemented"
+  FunctionDeclaration{} ->
+    throwError $ NotImplementedError "Function declarations are not yet implemented"
+  StructDeclaration{} ->
+    throwError $ NotImplementedError "Struct declarations are not yet implemented"
+  EnumDeclaration{} ->
+    throwError $ NotImplementedError "Enum declarations are not yet implemented"
+  StructInitialization{} ->
+    throwError $ NotImplementedError "Struct initialization is not yet implemented"
+  EnumElement{} ->
+    throwError $ NotImplementedError "Enum elements are not yet implemented"
+  StructElement{} ->
+    throwError $ NotImplementedError "Struct elements are not yet implemented"
+  CastToType{} ->
+    throwError $ NotImplementedError "Type casting is not yet implemented"
+  CastToIdentifier{} ->
+    throwError $ NotImplementedError "Identifier casting is not yet implemented"
+  SizeofType{} ->
+    throwError $ NotImplementedError "Sizeof operator is not yet implemented"
+  SizeofExpr{} ->
+    throwError $ NotImplementedError "Sizeof operator is not yet implemented"
+  Reference{} ->
+    throwError $ NotImplementedError "Reference operator is not yet implemented"
+  Dereference{} ->
+    throwError $ NotImplementedError "Dereference operator is not yet implemented"
+  Syscall{} ->
+    throwError $ NotImplementedError "Syscalls are not yet implemented"
+  AddEqOp{} ->
+    throwError $ NotImplementedError "Compound assignment operators are not yet implemented"
+  SubEqOp{} ->
+    throwError $ NotImplementedError "Compound assignment operators are not yet implemented"
+  MulEqOp{} ->
+    throwError $ NotImplementedError "Compound assignment operators are not yet implemented"
+  DivEqOp{} ->
+    throwError $ NotImplementedError "Compound assignment operators are not yet implemented"
+  _ -> throwError $ CompilationError "Unknown node type"
 
 -- Helper for compiling binary operations
 compileBinaryOp :: Node -> Node -> Instruction -> Compiler ()
